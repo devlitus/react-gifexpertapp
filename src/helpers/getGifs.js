0aa1380755de79fe84fc0dd31522ce41b69c4191
devlitus <developercarles@gmail.com>
2020-09-09 17:31:44 +0200

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=9v6eFZtAlSsbGZSl0GO9Aqe459QW9i8a`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
