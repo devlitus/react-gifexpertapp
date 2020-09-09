20a153e33815cd014b8e48e9e495eadc885f653a
devlitus <developercarles@gmail.com>
2020-09-09 17:32:46 +0200

    docs agregado

---
+++ docs/precache-manifest.407d4b43745bd219ae6948d30e03bc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2792d8c62e1fcc72b145535666bd1461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08119b1a6a7f1da409a",
+    "url": "/static/css/main.207bc459.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08119b1a6a7f1da409a",
+    "url": "/static/js/main.184e83de.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
